7eafc03e8786ec3b986a7c4b830907d5858cf909
xiangsxuan <598582847@qq.com>
2018-12-18 23:05:36 +0800

    Create stack.cpp

---
+++ DataStructure/stack.cpp
@@ -0,0 +1,38 @@
+//顺序栈stack
+//本处学习方法：把《大话数据结构》的main部分拷贝过来，其他部分自己实现
+//1.我的错误点有写备注，详看代码注释
+#include "stdio.h"    
+#include "stdlib.h"   
+#include "io.h"  
+#include "math.h"  
+#include "time.h"
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+#define MAXSIZE 20 
+
+typedef int Status;
+typedef int SElemType;
+
+int main()
+{
+	int j;
+	//SqStack s;
+	//int e;
+	//if (InitStack(&s) == OK)
+	//	for (j = 1; j <= 10; j++)
+	//		Push(&s, j);
+	//printf("栈中元素依次为：");
+	//StackTraverse(s);
+	//Pop(&s, &e);
+	//printf("弹出的栈顶元素 e=%d\n", e);
+	//printf("栈空否：%d(1:空 0:否)\n", StackEmpty(s));
+	//GetTop(s, &e);
+	//printf("栈顶元素 e=%d 栈的长度为%d\n", e, StackLength(s));
+	//ClearStack(&s);
+	//printf("清空栈后，栈空否：%d(1:空 0:否)\n", StackEmpty(s));
+
+	return 0;
+}
